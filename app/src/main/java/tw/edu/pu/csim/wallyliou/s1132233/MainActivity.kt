11a19fa1ed52814afbfc3adc2f0f7c21f825a949
wallyliou <wally950831@gmail.com>
2025-12-01 14:25:01 +0800

    v1_劉宇崴

--- app/src/main/java/tw/edu/pu/csim/wallyliou/s1132233/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/wallyliou/s1132233/MainActivity.kt
@@ -1,47 +1,52 @@
 package tw.edu.pu.csim.wallyliou.s1132233
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 import tw.edu.pu.csim.wallyliou.s1132233.ui.theme.S1132233Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        // 強迫直式螢幕
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        // 隱藏狀態列：獲取 WindowInsetsController，再隱藏statusBars
+        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+        windowInsetsController.hide(WindowInsetsCompat.Type.statusBars())
+
+
         setContent {
             S1132233Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                //GameScreen(message = "直式螢幕，隱藏狀態列")
+
+            }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello 劉宇崴!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    S1132233Theme {
-        Greeting("Android")
-    }
-}
+//@Composable
+//fun GameScreen(message: String) {
+    //Box(modifier = Modifier
+        //.fillMaxSize()
+        //.background(Color.Yellow)
+    //){
+        //Text(text = message)
+    //}
+//}
